0b1f541b387c99450039f2c67ebf88a2714890db
DanaeAguilar24 <aguilarflores.kenyadanae@gmail.com>
2025-11-06 12:43:30 -0600

    Primera Version

---
+++ src/main/java/Binario.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Binario {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+        System.out.println("Ingresa el no");
+        int cant = scanner.nextInt();
+
+    }
+}
